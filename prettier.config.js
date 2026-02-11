9c3412194a3cc711c9cbefa5fd43c5900ec158f6
Kerim Hadri <kerim.hadri@projekt8.net>
2026-02-11 18:06:05 +0100

    chore: install and configure tailwind prettier plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,14 @@
 //  @ts-check
 
-/** @type {import('prettier').Config} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   semi: false,
   singleQuote: true,
-  trailingComma: "all",
-};
+  trailingComma: 'all',
+  tailwindStylesheet: './src/styles/index.css',
+  tailwindAttributes: ['className'],
+  tailwindFunctions: ['clsx', 'cva', 'cn'],
+  plugins: ['prettier-plugin-tailwindcss'],
+}
 
-export default config;
+export default config
